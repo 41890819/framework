0796ae4d5f4afa37355f02dd7b49b70565000bce
zyang_bj <zyang_bj@ingenic.cn>
2015-01-27 14:56:28 +0800

    set SHOW_APP_STARTING_PREVIEW = false
    Change-Id: I727814767df1d8bc2be2e5da97c7226a459a1f6c

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -130,7 +130,7 @@ final class ActivityStack {
     
     // Set to false to disable the preview that is shown while a new activity
     // is being started.
-    static final boolean SHOW_APP_STARTING_PREVIEW = true;
+    static final boolean SHOW_APP_STARTING_PREVIEW = false;
     
     enum ActivityState {
         INITIALIZING,
