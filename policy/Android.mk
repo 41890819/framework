455cccc5be20bd9eb33e9a156bd24f9d66ecb029
Sunguoyun <sunny.gysun@ingenic.com>
2015-12-11 09:49:52 +0800

    ln /device/ingenic/xx/config/PhoneWindowManager.java to policy

--- policy/Android.mk
+++ policy/Android.mk
@@ -4,6 +4,13 @@ LOCAL_PATH:= $(call my-dir)
 # ============================================================
 include $(CLEAR_VARS)
 
+FILE := $(shell test -f $(LOCAL_PATH)/../../../device/ingenic/$(TARGET_PRODUCT)/config/PhoneWindowManager.java && echo yes)
+ifeq ($(FILE),yes)
+#$(warning "link PhoneWindowManager.java file $(TARGET_PRODUCT)")
+$(shell ln -f $(LOCAL_PATH)/../../../device/ingenic/$(TARGET_PRODUCT)/config/PhoneWindowManager.java $(LOCAL_PATH)/src/com/android/internal/policy/impl/PhoneWindowManager.java)
+endif
+
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
             
 LOCAL_MODULE := android.policy
