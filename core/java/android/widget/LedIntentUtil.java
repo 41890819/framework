b2af63b38a22689c9ca65dac1760f74a5dba87eb
pzhang_bj <Zona.pzhang@ingenic.com>
2016-05-31 17:07:33 +0800

    add new file core/java/android/widget/LedIntentUtil.java

---
+++ core/java/android/widget/LedIntentUtil.java
@@ -0,0 +1,13 @@
+package android.widget;
+
+public class LedIntentUtil {
+
+	/**
+	 * Broadcast Action: CameraDevice startPreview.
+	 */
+	public static final String ACTION_START_PREVIEW = "cn.ingenic.glass.ACTION_START_PREVIEW";
+	/**
+	 * Broadcast Action: CameraDevice stopPreview.
+	 */
+	public static final String ACTION_STOP_PREVIEW = "cn.ingenic.glass.ACTION_STOP_PREVIEW";
+}
